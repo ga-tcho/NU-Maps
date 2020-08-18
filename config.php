09a69820d6ea21ed3a4d669bd374fbd7e98bc5cd
Keijikotohab <50548395+Keijikotohab@users.noreply.github.com>
2020-08-18 18:45:52 +0900

    Add files via upload

--- config.php
+++ config.php
@@ -1,77 +1,110 @@
 <?php
 
-// The purpose of this central config file is configuring all examples
-// in one place with minimal work for your working environment
 
-$phpcas_path = '../../source/';
+/* -- 以下、基本の設定 ------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-///////////////////////////////////////
-// Basic Config of the phpCAS client //
-///////////////////////////////////////
 
-// Full Hostname of your CAS Server
-$cas_host = 'auth.nagoya-u.ac.jp';
+//【必須】 自分のメールアドレスの設定 -- 複数のメールアドレスに送信したい場合は、以下の行をコピーして増やしていけばOKです。行頭の//を消せば有効となります。いくつでも追加可能。 --
+$rm_send_address[] = 'kelincoco1108@icloud.com';
+$rm_send_address[] = 'ito.keiji@e.mbox.nagoya-u.ac.jp';
+$rm_send_address[] = 'tsurumi.kota@k.mbox.nagoya-u.ac.jp';
 
 
 
-// Context of the CAS Server
-$cas_context = '/cas';
 
-// Port of your CAS server. Normally for a https server it's 443
-$cas_port = 443;
-
-// Path to the ca chain that issued the cas server certificate
-//$cas_server_ca_cert_path = '/usr/local/share/ca-certificates/3some-cacert.crt';
-
-//////////////////////////////////////////
-// Advanced Config for special purposes //
-//////////////////////////////////////////
-
-// The "real" hosts of clustered cas server that send SAML logout messages
-// Assumes the cas server is load balanced across multiple hosts
-$cas_real_hosts = array (
-	'auth.nagoya-u.ac.jp'
-);
-
-// Database config for PGT Storage
-//$db = 'pgsql:host=localhost;dbname=phpcas';
-//$db = 'mysql:host=localhost;dbname=phpcas';
-//$db_user = 'phpcas';
-//$db_password = '1938r132rwv12cadl';
-//$db_user = 'root';
-//$db_password = 'test1234';
-//$db_table = 'phpcas';
-
-///////////////////////////////////////////
-// End Configuration -- Don't edit below //
-///////////////////////////////////////////
-
-// Generating the URLS for the local cas example services for proxy testing
-if ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on'){
-	$curbase = 'https://'.$_SERVER['SERVER_NAME'];
-}else{
-	$curbase = 'http://'.$_SERVER['SERVER_NAME'];
-}
-if ($_SERVER['SERVER_PORT'] != 80 && $_SERVER['SERVER_PORT'] != 443)
-	$curbase .= ':'.$_SERVER['SERVER_PORT'];
-
-$curdir = dirname($_SERVER['REQUEST_URI'])."/";
-
-// access to a single service
-$serviceUrl = $curbase.$curdir.'example_service.php';
-// access to a second service
-$serviceUrl2 = $curbase.$curdir.'example_service_that_proxies.php';
-
-$cas_url = 'https://'.$cas_host;
-if ($cas_port != '443')
-{
-	$cas_url = $cas_url.':'.$cas_port;
-}
-$cas_url = $cas_url.$cas_context;
+//【必須】 サンクスページのURL -- index.htmlからの相対パス、またはhttp://からの絶対パス --
+$rm_thanks_page_url = 'thanks.html';
 
 
-// Set the session-name to be unique to the current script so that the client script
-// doesn't share its session with a proxied script.
-// This is just useful when running the example code, but not normally.
-session_name('session_for:'.preg_replace('/[^a-z0-9-]/i', '_', basename($_SERVER['SCRIPT_NAME'])));
-?>
+
+
+
+
+
+
+/* -- 以下、自分に届くメールの設定 ------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+//【任意】 自分に届くメールの題名
+$rm_send_subject = 'メールフォームからお問い合わせがありました。';
+
+
+
+
+//【任意】 自分に届くメールの本文 -- EOMからEOM;までの間の文章を自由に変更してください。 --
+$rm_send_body = <<<EOM
+
+メールフォームからお問い合わせがありました。
+お問い合わせの内容は以下の通りです。
+
+EOM;
+
+
+
+
+
+
+
+
+/* -- 以下、相手への自動返信メールの設定 ------------------------------------------------------------------------------------------------------------------------------- */
+
+
+//【任意】 相手に自動返信メールを送るかどうか -- 送らない場合は0、送る場合は1にしてください。 --
+$rm_reply_mail = 1;
+
+
+
+
+//【だいたい必須】 メールの差出人名に表示される自分の名前 -- 相手への自動返信メールに使用されます --
+$rm_send_name = 'レスポンシブメールフォーム　差出人';
+
+
+
+
+//【任意】 相手に届く自動返信メールの題名
+$rm_thanks_subject = 'お問い合わせありがとうございました。';
+
+
+
+
+//【任意】 相手に届く自動返信メールの本文 -- EOMからEOM;までの間の文章を自由に変更してください。 --
+$rm_thanks_body  = <<<EOM
+
+この度はお問い合わせをいただき、ありがとうございました。
+折り返し担当者から返信が行きますので、しばらくお待ちください。
+以下の内容でお問い合わせをお受けいたしました。
+
+EOM;
+
+
+
+
+//【だいたい必須】 相手に届く自動返信メールの最後に付加される署名 -- EOMからEOM;までの間の文章を自由に変更してください。 --
+$rm_thanks_body_signature = <<<EOM
+
+この度はお問い合わせを頂き、重ねてお礼申し上げます。
+-----------------------------------------------------------------------------------
+
+　　レスポンシブメールフォーム
+　　〒***-**** ここに住所など
+　　TEL : ***-***-****
+　　Web Site URL : https://www.1-firststep.com
+　　Blog URL : https://www.firstsync.net
+
+-----------------------------------------------------------------------------------
+
+EOM;
+
+
+
+
+
+
+
+
+/* -- 以下、スパム対策機能 --------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+//【任意】 メールフォームを設置するサイトのドメイン -- 記入した場合はリファラチェック機能が有効になります。 --
+$rm_domain_name = '';
+//$rm_domain_name = '1-firststep.com';
